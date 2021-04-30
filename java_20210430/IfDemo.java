9041a7baca9e5cdcdf4d708bc55fe75d63ff0654
nothingct <46178551+nothingct@users.noreply.github.com>
2021-04-30 16:38:36 +0900

    Update IfDemo.java

--- java_20210430/IfDemo.java
+++ java_20210430/IfDemo.java
@@ -19,5 +19,21 @@ public class IfDemo {
 			season = "없는 계절";
 		}
 		System.out.println(month + "월은 " + season + "입니다.");
+
+		int a = 1259999;
+		double d = 1232324.569058;
+		String name = "오한승";
+		// printf 사용해보기
+		System.out.printf("a는 %,d입니다.\n", a); // 10자리 정수로 3자리마다 , 채운다.
+		System.out.printf("a는 %010d입니다.\n", a); // 10자리 정수로 0을 채우고
+		System.out.printf("d는 %,.2f입니다.\n", d); // 소숫점 이하 둘째자리까지 표현, 밑에는 반올림, 3자리마다 , 채운다.
+		System.out.printf("저는 %s입니다.\n", name);
+
+		// 삼항연산자 사용해보기
+		int a1 = 100;
+		// 조건식 ? 참일경우 실행 : 거짓일경우 실행
+		String msg = a1 % 2 == 0 ? "짝수" : "홀수";
+		System.out.println(msg);
+		
 	}
 }
